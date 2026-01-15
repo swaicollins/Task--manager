d02210e3b64b4cff454e8037b7658860e3cb5f12
Collins Swai <swailerc23@gmail.com>
2026-01-15 09:17:11 +0300

    Task model class

---
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -0,0 +1,65 @@
+package com.example.taskmanager.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @Size(max = 255)
+    private String description;
+    private boolean completed;
+
+
+    public Task() {
+    }
+
+    public Task(String title, String description, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
